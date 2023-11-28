8929b2647a07099f34778a6a71998cd526d89df8
Richard Zhu <richardzhu78@gmail.com>
2023-11-28 14:49:27 -0500

    Fix presto query sql dirs

--- data_ingest/movies_fx_tables.sql
+++ data_ingest/movies_fx_tables.sql
@@ -1,7 +1,7 @@
 CREATE EXTERNAL TABLE movies (title string, year int, month int, duration int, genres string, countries string, rating double, votes int, budget bigint, boxoffice bigint, currency string)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-STORED AS TEXTFILE LOCATION 'hdfs://nyu-dataproc-m/user/bay2006_nyu_edu/hw8/output/';
+STORED AS TEXTFILE LOCATION 'hdfs://nyu-dataproc-m/user/rz2123_nyu_edu/project/data/movies/clean';
 
 CREATE EXTERNAL TABLE fx (Year string, Month string, EUR string, JPY string, BGN string, CZK string, DKK string, GBP string, HUF string, PLN string, RON string, SEK string, CHF string, ISK string, NOK string, HRK string, RUB string, TRL string, TRY string, AUD string, BRL string, CAD string, CNY string, HKD string, IDR string, ILS string, INR string, KRW string, MXN string, MYR string, NZD string, PHP string, SGD string, THB string, ZAR string)
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
-STORED AS TEXTFILE LOCATION 'hdfs://nyu-dataproc-m/user/bay2006_nyu_edu/proj/fxdata/';
+STORED AS TEXTFILE LOCATION 'hdfs://nyu-dataproc-m/user/rz2123_nyu_edu/project/data/fx/clean';
