5d1d878b7144b33d50b6c507c409f23701345640
byang20 <53458555+byang20@users.noreply.github.com>
2023-12-03 16:29:44 -0500

    update nullfill.scala

--- src/etl_code/brian/nullfill.scala
+++ src/etl_code/brian/nullfill.scala
@@ -1,13 +1,14 @@
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions._
 
-
 // Load CSV file into a DataFrame
-val inputFilePath = "hdfs://nyu-dataproc-m/user/bay2006_nyu_edu/nullfill/IMDb-movies.csv"
-val outputFilePath = "nullfill/nullfill-IMDb-movies"
+val inputFilePath = "hdfs://nyu-dataproc-m/user/rz2123_nyu_edu/project/data/movies/input/IMDb-movies.csv"
+val outputFilePath = "hdfs://nyu-dataproc-m/user/rz2123_nyu_edu/project/data/movies/etl/nullfill"
 
-val csvOptions = Map("header" -> "true", "inferSchema" -> "true", "quote" -> "\"", "escape" -> "\"")
+var csvOptions = Map("header" -> "true", "inferSchema" -> "true", "quote" -> "\"", "escape" -> "\"")
 var df: DataFrame = spark.read.options(csvOptions).csv(inputFilePath)
+
+println(s"Input successfully read from $inputFilePath")
 
 val stringColumns = df.columns.filter(colName => df.schema(colName).dataType == org.apache.spark.sql.types.StringType)
 val numericColumns = df.columns.filter(colName => df.schema(colName).dataType.isInstanceOf[org.apache.spark.sql.types.NumericType])
@@ -18,8 +19,14 @@ df = numericColumns.foldLeft(df)((df, colName) => df.withColumn(colName, when(co
 
 df = df.coalesce(1)
 
+csvOptions = Map("header" -> "false", "inferSchema" -> "true", "quote" -> "\"", "escape" -> "\"")
+
 // Save the result as a CSV file
 df.write
   .mode("overwrite")  // Overwrite the output file if it already exists
   .options(csvOptions)
   .csv(outputFilePath)
+
+println(s"Output successfully written to $outputFilePath")
+
+System.exit(0)
